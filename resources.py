3cdc1210c1eedcad22465d1a2005e8abecc0af89
mdkv <m.a.h.de.kok@student.vu.nl>
2023-02-21 16:54:05 +0100

    Updated graph based on feedback from meeting. Also removed the class '0'

---
+++ resources.py
@@ -0,0 +1,10 @@
+import uuid
+from rdflib import URIRef
+
+
+def node_creation(path, entity_mention, base_add=''):
+    base = f"http://kflow.eurecom.fr{base_add}"
+    uri = base + '/' + str(uuid.uuid5(uuid.NAMESPACE_DNS, path + entity_mention))
+    return URIRef(uri)
+
+
