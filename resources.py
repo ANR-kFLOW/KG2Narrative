08322be2820b242da14edebf55fd5955f9ce6bc2
mdkv <m.a.h.de.kok@student.vu.nl>
2023-06-08 15:16:32 +0200

    Added code to process the output of the graph search method, and convert it to input for the text generation model

--- resources.py
+++ resources.py
@@ -1,8 +1,38 @@
 import uuid
 from rdflib import URIRef
+from urllib.parse import urlparse
 
 
 def node_creation(path, entity_mention, base_add=''):
+    """
+    :param path: A path that has to be added to the generated URI e.g. /sentence/
+    :param entity_mention: The entity for which the generation is done
+    :param base_add: Allows for more information to be added to the generated URI
+    :return:
+    """
     base = f"http://kflow.eurecom.fr{base_add}"
     uri = base + '/' + str(uuid.uuid5(uuid.NAMESPACE_DNS, path + entity_mention))
     return URIRef(uri)
+
+
+def uri_validator(x):
+    """
+    :param x: The URI/ URL to check
+    :return:
+    """
+    try:
+        result = urlparse(x)
+        return all([result.scheme, result.netloc])
+    except:
+        return False
+
+
+def mapping_dict(words):
+    """
+    :param words: The words for which an encoding has to be made
+    :return: A dictionary that performs the translation
+    """
+
+    mapping_dict = dict([(y, x + 1) for x, y in enumerate(sorted(set(words)))])
+
+    return mapping_dict
